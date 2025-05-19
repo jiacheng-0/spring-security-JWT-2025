4b9ec4baa2c3fd70af2e14195b0c061bf7ec3e9a
Teo Jia Cheng <jcteo.2018@scis.smu.edu.sg>
2025-05-19 17:48:43 +0800

    init

---
+++ src/main/java/com/example/spring_security_JWT_2025/SpringSecurityJwt2025Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_JWT_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwt2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwt2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_security_JWT_2025/SpringSecurityJwt2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_JWT_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwt2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
